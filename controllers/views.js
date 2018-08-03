c4767aeb45c4cc5d92ec0f2244e2808ea083ce0d
hant <se7en08192@gmail.com>
2018-08-03 09:05:51 +0700

    hant

--- controllers/views.js
+++ controllers/views.js
@@ -1,72 +1,84 @@
 let router = require("express").Router();
 
+//Tro den file constant
+let constant = require("../datautils/constant.js");
+
 router.get("/", (req, res) => {
-    res.render("index");
+  res.render("index", {
+    TLBB: constant.TLBB,
+    TLBBUP: constant.TLBBUP,
+    NAMEUP: constant.NAMEUP,
+    NAME: constant.NAME
+  });
 });
 
 router.get("/account", (req, res) => {
-    res.render("account/index");
+  res.render("account/index", {
+    TLBB: constant.TLBB,
+    TLBBUP: constant.TLBBUP,
+    NAMEUP: constant.NAMEUP,
+    NAME: constant.NAME
+  });
 });
 
 router.get("/account/register", (req, res) => {
-    res.render("account/directional/register.ejs");
+  res.render("account/directional/register.ejs");
 });
 
 router.get("/account/login", (req, res) => {
-    res.render("account/directional/login.ejs");
+  res.render("account/directional/login.ejs");
 });
 
 router.get("/account/account-info", (req, res) => {
-    res.render("account/directional/account-info.ejs");
+  res.render("account/directional/account-info.ejs");
 });
 
 router.get("/account/ratings-level", (req, res) => {
-    res.render("account/directional/ratingslevel.ejs");
+  res.render("account/directional/ratingslevel.ejs");
 });
 
 router.get("/account/ratings-wealth", (req, res) => {
-    res.render("account/directional/ratingswealth.ejs");
+  res.render("account/directional/ratingswealth.ejs");
 });
 
 router.get("/account/change-password", (req, res) => {
-    res.render("account/directional/change-password.ejs");
+  res.render("account/directional/change-password.ejs");
 });
 
 router.get("/account/add-coin", (req, res) => {
-    res.render("account/directional/add-coin.ejs");
+  res.render("account/directional/add-coin.ejs");
 });
 
 router.get("/account/unlock-char", (req, res) => {
-    res.render("account/directional/unlock-char.ejs");
+  res.render("account/directional/unlock-char.ejs");
 });
 
 router.get("/account/fix-map", (req, res) => {
-    res.render("account/directional/fix-map.ejs");
+  res.render("account/directional/fix-map.ejs");
 });
 
 router.get("/account/reset-time", (req, res) => {
-    res.render("account/directional/reset-time.ejs");
+  res.render("account/directional/reset-time.ejs");
 });
 
 router.get("/account/reset-password", (req, res) => {
-    res.render("account/directional/reset-password.ejs");
+  res.render("account/directional/reset-password.ejs");
 });
 
 router.get("/account/dice", (req, res) => {
-    res.render("account/directional/dice.ejs");
+  res.render("account/directional/dice.ejs");
 });
 
 router.get("/admin/add-news-events", (req, res) => {
-    res.render("admin/directional/add-news-events.ejs");
+  res.render("admin/directional/add-news-events.ejs");
 });
 
 router.get("/admin/news-events", (req, res) => {
-    res.render("admin/directional/news-events.ejs");
+  res.render("admin/directional/news-events.ejs");
 });
 
 router.get("/not-found", (req, res) => {
-    res.render("not-found");
+  res.render("not-found");
 });
-
 
 module.exports = router;
